82138205d9ea918d2d43d14de67bdf816a13dd50
gvvnraeb <dann.nn49825@gmail.com>
2020-11-29 17:22:05 +0800

    .

--- src/App.java
+++ src/App.java
@@ -1,9 +1,36 @@
 import java.sql.*;
+import java.util.*;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+		
 		connect();
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Welcome! Who are you?");
+		System.out.println("1. An administrator");
+		System.out.println("2. A passenger");
+		System.out.println("3. A driver");
+		System.out.println("4. A manager");
+		System.out.println("5. None of the above");
+		System.out.println("Please enter [1-4]");
+		
+		int input;
+		try {
+			input = keyboard.nextInt();
+		} catch (Exception e) {
+			System.out.println("Invalid input! Please try again.");
+		} finally {
+			switch (input) {
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+			}
+		}
+		
 	}
 
 	public static Connection connect() {
