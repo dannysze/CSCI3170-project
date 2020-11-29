2e76863a742c14433f137a0c4fd824abf5f9f31e
gvvnraeb <dann.nn49825@gmail.com>
2020-11-29 15:45:09 +0800

    Connect

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.sql.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello, World!");
+	}
+
+	public static Connection connect() {
+		String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/group13";
+		String dbUsername = "Group13";
+		String dbPassword = "3170group13";
+
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+			System.out.println("Connected");
+		} catch (ClassNotFoundException e) {
+			System.out.println("[ERROR]: Java MySQL Driver not found!!");
+			System.exit(1);
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
 }
