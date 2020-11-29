91a60be8c75ce69cb2b3d2d2cd3d2f3dda20311e
gvvnraeb <dann.nn49825@gmail.com>
2020-11-29 21:47:19 +0800

    .

--- src/App.java
+++ src/App.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.sql.*;
 import java.util.*;
+import roles.*;
 
 public class App {
 	public static void main(String[] args) throws Exception {
 		
-		// Connection conn = connect();
+		Connection conn = connect();
 		
-		// Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
 
-		// System.out.println("Welcome! Who are you?");
-		// System.out.println("1. An administrator");
-		// System.out.println("2. A passenger");
-		// System.out.println("3. A driver");
-		// System.out.println("4. A manager");
-		// System.out.println("5. None of the above");
-		// System.out.println("Please enter [1-4]");
+		System.out.println("Welcome! Who are you?");
+		System.out.println("1. An administrator");
+		System.out.println("2. A passenger");
+		System.out.println("3. A driver");
+		System.out.println("4. A manager");
+		System.out.println("5. None of the above");
+		System.out.println("Please enter [1-4]");
 		
-		// int input = 0;
-		// try {
-		// 	input = keyboard.nextInt();
-		// } catch (Exception e) {
-		// 	System.out.println("Invalid input! Please try again.");
-		// } finally {
-		// 	switch (input) {
-		// 		case 1: break;
-		// 		case 2: break;
-		// 		case 3: break;
-		// 		case 4: break;
-		// 		case 5: break;
-		// 		default: 	System.out.println("Invalid input! Please try again.");
-		// 							break;
-		// 	}
-		// }
+		int input = 0;
+		try {
+			input = keyboard.nextInt();
+		} catch (Exception e) {
+			System.out.println("Invalid input! Please try again.");
+		} finally {
+			switch (input) {
+				case 1: break;
+				case 2: break;
+				case 3: break;
+				case 4: break;
+				case 5: break;
+				default: 	System.out.println("Invalid input! Please try again.");
+									break;
+			}
+		}
 		
 
 		// CreateTable();
 		// post();
-		createTables();
+		// createTables();
 	}
 
 	public static Connection connect() {
@@ -89,45 +90,6 @@ public class App {
 			System.out.println("VendorError: " + e.getErrorCode());
 		} finally {
 			System.out.println("Date inserted successfully!");
-		}
-	}
-
-	public static void createTables() throws Exception {
-
-		Connection con = connect();
-		try {
-			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS Drivers(ID integer PRIMARY KEY, Name varchar(30) not null, Vehicle_ID varchar(6) not null, Driving_years integer);");
-			create.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-		} finally {
-			System.out.println("Table created successfully!");
-		}
-
-		String filename = "./test_data/test_data/drivers.csv";
-		File file = new File(filename);
-		try {
-			Scanner inputStream = new Scanner(file);
-			PreparedStatement insert = null;
-			while (inputStream.hasNext()) {
-				String data = inputStream.nextLine();
-				String[] values = data.split(",");
-				int id = Integer.parseInt(values[0]);
-				int drivingYears = Integer.parseInt(values[3]);
-				insert = con.prepareStatement("INSERT INTO Drivers VALUES ("+id+", '"+values[1]+"', '"+values[2]+"', "+drivingYears+")");
-				insert.executeUpdate();
-			}
-			inputStream.close();
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (SQLException e) {
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-		} finally {
-			System.out.println("Drivers tables are created");
 		}
 	}
 }
--- src/roles/Administrator.java
+++ src/roles/Administrator.java
@@ -1,5 +1,48 @@
 package roles;
+import java.util.*;
+import java.io.*;
+import java.sql.*;
 
 public class Administrator {
   
+	public static void createTables() throws Exception {
+
+		Connection con = App.connect();
+		try {
+			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS Drivers(ID integer PRIMARY KEY, Name varchar(30) not null, Vehicle_ID varchar(6) not null, Driving_years integer);");
+			create.executeUpdate();
+      create.close();
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
+		} finally {
+			System.out.println("Table created successfully!");
+		}
+
+		String filename = "./test_data/test_data/drivers.csv";
+		File file = new File(filename);
+		try {
+			Scanner inputStream = new Scanner(file);
+			PreparedStatement insert = null;
+			while (inputStream.hasNext()) {
+				String data = inputStream.nextLine();
+				String[] values = data.split(",");
+				int id = Integer.parseInt(values[0]);
+				int drivingYears = Integer.parseInt(values[3]);
+				insert = con.prepareStatement("INSERT INTO Drivers VALUES ("+id+", '"+values[1]+"', '"+values[2]+"', "+drivingYears+")");
+        insert.executeUpdate();
+        insert.close();
+			}
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
+		} finally {
+			System.out.println("Drivers tables are created");
+		}
+	}
 }
